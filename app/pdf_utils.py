3c79741e3de8c94f18a8ae676e505a167303deac
vanshtyagij <vanshtyagij2@gmail.com>
2026-08-14 02:01:10 +0530

    Delete app directory

--- app/pdf_utils.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-
-def extract_pdf_text(uploaded_file) -> str:
-    reader = PdfReader(uploaded_file)
-    pages = []
-    for page in reader.pages:
-        pages.append(page.extract_text() or "")
-    return "\n\n".join(pages)
